bb2a4d17d1ea444d0d11c444579a71062a42a79c
Bastien Nizart <bastien@nizart.me>
2025-10-03 11:24:25 +0200

    feat(seo): add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bg2s.com",
   vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
     build: {
       sourcemap: false,
     },
   },
-  integrations: [icon()],
+  integrations: [
+    icon(),
+    sitemap(),
+  ],
 });
